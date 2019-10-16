fe2ce633f8cc9e567ba71092affd9920636b0c73
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-16 20:56:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map (sourceArray , callingback){
+  let newArr =[];
+  for (let i = 0; i < sourceArray.length; i++) {
+    let el= sourceArray[i]
+    newArr.push(callingback(el))
+  }
+
+  return newArr;
+}
+function reduce(sourceArray,point,callingback){
+  
+  for (let i = 0; i < sourceArray.length; i++) {
+  }
+}
