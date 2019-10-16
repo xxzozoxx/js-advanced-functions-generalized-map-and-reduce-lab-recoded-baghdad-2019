7214482722e857c38621b8a9ea6c1c2a8cc13ce6
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-10-16 21:14:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map (sourceArray , callingback){
+  let newArr =[];
+  for (let i = 0; i < sourceArray.length; i++) {
+    let el= sourceArray[i]
+    newArr.push(callingback(el))
+  }
+
+  return newArr;
+}
+function reduce(sourceArray,callingback,point){
+  let result;
+  if(!!point){
+    result = point
+  } 
+  else{
+   
+     result = sourceArray[0]
+  }
+  let i = (!!point) ? 0 : 1
+  for (; i < sourceArray.length; i++) {
+    result = callingback(sourceArray[i],result);
+  }
+return result
+}
